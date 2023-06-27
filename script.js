ad23ab5cb901fa74fd74a322765332ef69c4b3b6
Saajan Singh Rana <123057148+saajanrana@users.noreply.github.com>
2023-06-27 04:06:08 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
-//your code here
-function findMajorityElement(arr) {
-    var count = 0;
-    var candidate = null;
-
-    // Finding the candidate for majority element
-    for (var i = 0; i < arr.length; i++) {
-        if (count === 0) {
-            candidate = arr[i];
-            count = 1;
-        } else if (arr[i] === candidate) {
-            count++;
-        } else {
-            count--;
-        }
+ function majorityElementChecker(arr)
+    {
+        let map = new Map();
+ 
+        for(let i = 0; i < arr.length; i++) {
+            if (map.has(arr[i])) {
+                    let count = map.get(arr[i]) +1;
+                    if (count > arr.length /2) {
+                      //  document.write("Majority found :- " + arr[i]);
+                        return arr[i];
+                    } else
+                        map.set(arr[i], count);
+ 
+            }
+            else
+                map.set(arr[i],1);
+            }
+            return 1;
+            //document.write(" No Majority element");
     }
 
-    // Verifying if the candidate is the majority element
-    count = 0;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === candidate) {
-            count++;
-        }
-    }
-
-    // Checking if the candidate appears more than floor(n/2) times
-    if (count > Math.floor(arr.length / 2)) {
-        return candidate;
-    } else {
-        // Majority element always exists, so this line won't be reached
-        return -1;
-    }
-}
-
-// Example usage
-var inputArray = [2, 1, 2];
-var majorityElement = findMajorityElement(inputArray);
-console.log(majorityElement);
+module.exports = majorityElementChecker;
